0625cd4cb948e1fa7d5ab8cfb3e04f6eaf06e89d
wanguanfu <wangguan592@sina.cn>
2019-08-23 16:05:21 +0800

    init

---
+++ my-app/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+           {{msg}}
+            <div>
+                <input type="text" v-model="info">
+                <button @click="handlerClick">添加</button>
+            </div>
+            <ul>
+                <todo-item v-for="item in list" :key="item">  
+                    <template v-slot:item="itemProps">
+                        <span :style="{fontSize: '20px',color:itemProps.checked ? 'red':'blue'}">{{item}}</span>
+                    </template>
+                </todo-item>
+            </ul>
+   </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem'
+export default {
+  name: 'app',
+  data(){
+      return {
+          msg:"hello geek",
+          info: '',
+          list: [],
+      }
+
+  },
+  methods: {
+      handlerClick() {
+          this.list.push(this.info)
+          this.info=''
+      }
+  },
+  components: {
+    TodoItem
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ my-app/src/components/TodoItem.vue
@@ -0,0 +1,28 @@
+<template>
+    <li class="item">
+        <input type="checkbox" v-model="checked">
+        <slot name='item' v-bind="{checked}"></slot>
+
+           <!-- <template v-slot:item="itemProps">
+                        <span :style="{fontSize: '20px',color:itemProps.checked ? 'red':'blue'}">{{item}}</span>
+            </template> -->
+    </li>
+</template>
+
+
+<script>
+export default {
+    props: ['item'],
+    data(){
+        return {
+            checked :false
+        }
+    }
+}
+</script>
+
+<style scope>
+    .item {
+        color: red
+    }
+</style>
